225a6e919998be59d3fe7b41e5f6f04d46238b30
Nan Zhang <zhangnan2920214@gmail.com>
2014-06-08 23:24:47 -0700

    tweak yahoo code

--- src/yahoo/RandBSTNodeGen.java
+++ src/yahoo/RandBSTNodeGen.java
@@ -1,11 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Nan Zhang.
+ * 
+ *        Filename:   RandBSTNodeGen.java
+ *         Version:   1.0
+ *         Created:   6/8 
+ *          Author:   Nan Zhang 
+ *    Organization:   https://github.com/Nan-Zhang
+ *            Note:   
+ *            
+ * All rights reserved.
+ ******************************************************************************/
+
 package yahoo;
 
 public class RandBSTNodeGen {
-    public Node{
-        int val;
-        Node left;
-        Node right;
-        int leftNum;
+    
+    public static class Node{
+        public int val;
+        public Node left;
+         public Node right;
+public         int leftNum;
         
         public Node(int val){
            this.val = val;
@@ -38,68 +52,7 @@ public class RandBSTNodeGen {
               }
           }
     }
-    
-    
-    
-
-Q. Return a random node from a binary search tree
-
-            A
-          /   \
-         B     C
-        / \   / \
-       D   E F   G
-        \   
-         H
-
-Solution 1: Traverse tree, put nodes in list, generate random index
-
-public Node{
-    int val;
-    Node left;
-    Node right;
-    int leftNum;
-    
-    public Node(int val){
-       this.val = val;
-    }
-}
-
-public Node generate(Node root, int k){
-       if(k <= root.leftNum){
-            return generate(root.left, k);
-       }else if(k == root.leftNum + 1){
-            return root;
-       }else{
-            return generate(root.right, k - (root.leftNum + 1));
-       }
-}
-
-public void insert(Node root, Int val){
-      if(val <= root.val){
-           root.leftNum++;
-           if(root.left != null){
-               insert(root.left, val);
-           }else{
-               root.left = new Node(val);
-           }
-      }else{
-          if(root.right != null){
-              insert(root.right, val);
-          }else{
-              root.right = new Node(val);
-          }
-      }
-}
-
-
-        10
-    5       15
-  1   7   12
-
-
-
-10 5 1 7 15 12
+   
 
 public Node{
    int val;
@@ -145,9 +98,4 @@ public Node construct(int[] input){
    return constructRec(input, input[0], 0, Integer.MIN_VALUE, Integer.MAX_VALUE, input.length);
 }
 
-
-
-    public static void main(String[] args) {
-        
-    }
 }
